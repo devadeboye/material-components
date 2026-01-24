42356a16d32552a6a5002a2c2e4a2fa7d23be68d
Adeboye Emmanuel <emmanueladeboye2017@gmail.com>
2026-01-24 19:45:51 +0100

    fix: resolve issue with typescript trying to generate type for css files

--- packages/react-material-components/tsup.config.ts
+++ packages/react-material-components/tsup.config.ts
@@ -1,14 +1,20 @@
 import { defineConfig } from "tsup";
 
-export default defineConfig({
-	entry: ["src/index.ts", "src/styles.css", "src/base.css"],
-	format: ["cjs", "esm"],
-	dts: true,
-	splitting: false,
-	sourcemap: true,
-	clean: true,
-	external: ["react", "react-dom"],
-	banner: {
-		js: '"use client";',
+export default defineConfig([
+	{
+		entry: ["src/index.ts"],
+		format: ["cjs", "esm"],
+		dts: true,
+		splitting: false,
+		sourcemap: true,
+		clean: true,
+		external: ["react", "react-dom"],
+		banner: {
+			js: '"use client";',
+		},
 	},
-});
+	{
+		entry: ["src/styles.css"],
+		clean: false,
+	},
+]);
