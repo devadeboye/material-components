3343f425c09c7c1f85038d12d5ed76025e164439
Adeboye Emmanuel <emmanueladeboye2017@gmail.com>
2026-01-24 19:35:27 +0100

    fix: ensure styles are packaged

--- packages/react-material-components/tsup.config.ts
+++ packages/react-material-components/tsup.config.ts
@@ -1,14 +1,14 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/index.ts"],
-  format: ["cjs", "esm"],
-  dts: true,
-  splitting: false,
-  sourcemap: true,
-  clean: true,
-  external: ["react", "react-dom"],
-  banner: {
-      js: '"use client";',
-  },
+	entry: ["src/index.ts", "src/styles.css"],
+	format: ["cjs", "esm"],
+	dts: true,
+	splitting: false,
+	sourcemap: true,
+	clean: true,
+	external: ["react", "react-dom"],
+	banner: {
+		js: '"use client";',
+	},
 });
